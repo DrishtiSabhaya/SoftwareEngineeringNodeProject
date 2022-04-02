5a77f8d090487605a4b986140aa59b54e23d538e
DrishtiSabhaya <drishtisabhaya@gmail.com>
2022-04-02 15:09:05 -0400

    server modified2

--- server.ts
+++ server.ts
@@ -39,7 +39,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     credentials: true,
-    origin: '*'
+    origin: 'http://localhost:3000'
 }));
 
 const SECRET = 'process.env.SECRET';
